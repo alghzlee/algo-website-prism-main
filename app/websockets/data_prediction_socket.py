aaa4158fcb3834c48812d4e7d61fdbddcb1dc03f
alghzlee <yahyazali01@gmail.com>
2025-12-30 17:54:31 +0700

    fix bug prediction page

--- app/websockets/data_prediction_socket.py
+++ app/websockets/data_prediction_socket.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_socketio import emit
 from app.services.data_prediction_service import generate_heart_rate, generate_oxygen_saturation, generate_respiratory_rate
 from app.extensions import socketio
@@ -7,14 +7,23 @@ import time
 dataPredictionSocketio = Blueprint('data_prediction', __name__)
 global_time = 0
 
+# Track active sessions
+active_prediction_sessions = set()
+
 @socketio.on('connect', namespace='/vital_patient_prediction')
 def handle_connect():
+    active_prediction_sessions.add(request.sid)
     emit('message', {'status': 'Connected to data vital prediction'})
+
+@socketio.on('disconnect', namespace='/vital_patient_prediction')
+def handle_disconnect():
+    active_prediction_sessions.discard(request.sid)
 
 @socketio.on('get_data_prediction_vital_patient', namespace='/vital_patient_prediction')
 def handle_get_data():
     global global_time
-    while True:
+    sid = request.sid
+    while sid in active_prediction_sessions:
         heart_rate = generate_heart_rate(global_time)
         oxygen_saturation = generate_oxygen_saturation(global_time)
         respiratory_rate = generate_respiratory_rate(global_time)
@@ -27,4 +36,4 @@ def handle_get_data():
         emit('data_prediction_vital_patient', data, namespace='/vital_patient_prediction')
 
         global_time += 1
-        time.sleep(5)  
+        socketio.sleep(5)  
